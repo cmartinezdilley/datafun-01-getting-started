5a3f3da79de00de939f6b39155426ca217702b67
Christine Martinez <nistine@gmail.com>
2023-01-24 17:29:13 -0600

    Project 1

--- user_script.py
+++ user_script.py
@@ -1,3 +1,23 @@
-"""
-Complete the script.
-"""
+title=input("Enter title: ")
+
+print(title)
+
+cat1 = 10
+cat2 = 5.5
+cat3 = 15
+
+print('Sum is', cat1 + cat2 + cat3)
+
+numbers =[10,5.5,15]
+average=sum(numbers)/len(numbers)
+print("Average of list:", round(average))
+
+print(cat1*cat2*cat3)
+
+print(min(cat1, cat3))
+print(max(cat1, cat3))
+
+print('Range:', min(10,5.5,15), '_',
+max(10,5.5,15))
+
+print("The oldest cat is 15 years old!")
